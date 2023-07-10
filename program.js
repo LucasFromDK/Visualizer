7db0ab5160cb00d77adcd9a5b92e34782d474037
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-07-10 17:26:39 +0200

    Update

--- program.js
+++ program.js
@@ -3,20 +3,27 @@ let soundName = [];
 let currentName = 0
 let sound = [];
 let currentSound = 0;
+let songFinished = false
 let amplitude;
 let fft;
 
 function preload() {
 sound[0] = loadSound("src/sounds/NCS_Skyhigh.mp3");
-soundName[0] = "NCS Skyhigh"
+soundName[0] = "NCS - Skyhigh"; //Electronic / House
 sound[1] = loadSound("src/sounds/Rift.mp3");
-soundName[1] = "NCS Rift"
+soundName[1] = "NCS - Rift"; //Electronic / Drum & Bass
 sound[2] = loadSound("src/sounds/Drop In The Ocean.mp3");
-soundName[2] = "NCS Drop In The Ocean"
+soundName[2] = "NCS - Drop In The Ocean"; //Electronic
 sound[3] = loadSound("src/sounds/EXXO Tension NCS.mp3");
-soundName[3] = "NCS EXXO Tension"
+soundName[3] = "NCS EXXO - Tension"; //Future House
 sound[4] = loadSound("src/sounds/ANGELPLAYA PULL UP.mp3");
-soundName[4] = "NCS ANGELPLAYA Pull Up"
+soundName[4] = "NCS ANGELPLAYA - Pull Up"; //Phonk
+sound[5] = loadSound("src/sounds/Razihel Faster.mp3")
+soundName[5] = "NCS Razihel - Faster" //Phonk
+sound[6] = loadSound("src/sounds/ATSMXN, XTOM - The Raven.mp3")
+soundName[6] = "NCS ATSMXN, XTOM - The Raven" //Phonk
+sound[7] = loadSound("src/sounds/VOLT VISION, Beneath My Shade - Dangerous.mp3")
+soundName[7] = "NCS VOLT VISION, Beneath My Shade - Dangerous"
 }
 
 function setup() {
@@ -48,7 +55,16 @@ function draw() {
   text("Playback Rate: " + rateSlider.value(), volumeSlider.x+400, playButton.y+14)
   soundInfo = fft.analyze();
   fft.smooth(0.9);
-  text("Current Song: " + soundName[currentName], playButton.x, playButton.y-5)
+
+  if(sound[currentSound].isPlaying()) {
+    text("Current Song: " + soundName[currentName], playButton.x, playButton.y-5) 
+  } else if (!sound[currentSound].isPlaying() && playButton.html() == "Pause" || songFinished == true){
+    text("Finished Song: " + soundName[currentName], playButton.x, playButton.y-5)
+    playButton.html("Play")
+    songFinished = true
+  } else if (!sound[currentSound].isPlaying() && playButton.html() == "Play" && songFinished == false) {
+    text("Paused Song: " + soundName[currentName], playButton.x, playButton.y-5)
+  }
   
 for(let i = 0; i < soundInfo.length; i++) {
   noFill();
@@ -62,29 +78,26 @@ for(let i = 0; i < soundInfo.length; i++) {
 }
 
 function toggleMusic() {
+  songFinished = false
   if(sound[currentSound].isPlaying()) {
     sound[currentSound].pause();
-    playButton.html("Play");
   } else {
     sound[currentSound].play();
     playButton.html("Pause");
-    console.log(sound[currentSound]);
   }
 }
 
 function playNext() {
+  songsongFinished = false
   soundName[currentName]
   sound[currentSound].stop();
   if(currentSound == sound.length-1) {
     currentSound = 0;
     currentName = 0
-    console.log(soundName[currentName])
   } else {
     currentSound++;
     currentName++;
-    console.log(soundName[currentName])
   }
   playButton.html("Pause");
   sound[currentSound].play();
-  console.log(sound[currentSound]);
 }
